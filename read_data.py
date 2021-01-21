e91c6ac095928bb4b12f49d15f685fed75ed2722
MaukWM <maukmul@gmail.com>
2021-01-21 16:46:39 +0100

    prediction code added

--- read_data.py
+++ read_data.py
@@ -15,7 +15,7 @@ pd.set_option("max_colwidth", None)
 # fietsdata = pd.read_csv("data/fiets_1_maart_5_april_minuut.csv")
 # fietsdata1 = pd.read_csv("data/fiets_1_maart_5_april_uur.csv")
 # fietsdata2 = pd.read_csv("data/fiets_27_april_31_mei_minuut.csv")
-fietsdata3 = pd.read_csv("data/fiets_27_april_31_mei_uur.csv")
+fietsdata3 = pd.read_csv("data/fiets_27_april_31_mei_uur.csv", nrows=24*7)
 # print(fietsdata)
 
 fietsdata3 = fietsdata3.loc[fietsdata3['naam'] == "Lange Kleiweg (westzijde) tussen Delft en Rijswijk (thv A4)"]
--- rnn_model.py
+++ rnn_model.py
@@ -23,29 +23,21 @@ import matplotlib.pyplot as plt
 
 class Model:
 
-    def __init__(self, data=None, batch_size=256, state_size=10, input_feature_amount=10, output_feature_amount=1,
+    def __init__(self, training_data=None, validation_data=None, batch_size=256, state_size=1, input_feature_amount=10, output_feature_amount=1,
                  seq_len_in=24, seq_len_out=24, steps_per_epoch=100, epochs=20, learning_rate=0.00075):
 
-        self.data = data
+        self.training_data = training_data
+        self.validation_data = validation_data
         self.batch_size = batch_size
         self.state_size = state_size
-        self.input_feature_amount = input_feature_amount
-        self.output_feature_amount = output_feature_amount
         self.seq_len_in = seq_len_in
         self.seq_len_out = seq_len_out
 
-        # TODO: set data to generator
-
-        # Create input dummy set, with 100 datapoints, of 24 hours, with 50 input features
-        self.input_data = np.zeros((100, 24, 50)) + 1
-        # Create output dummy set, with 100 datapoints, of 24 hours, with 1 output feature
-        self.output_data = np.zeros((100, 24, 1)) + 2
-
         self.test_train_ratio = 0.5
 
-        # TODO: Split dynamically based on length data
-        self.train_x, self.test_x = self.input_data[:80, :], self.input_data[80:, :]
-        self.train_y, self.test_y = self.output_data[:80, :], self.output_data[80:, :]
+        self.train_x, self.train_y = training_data
+        self.test_x, self.test_y = validation_data
+        self.input_shape = (self.train_x.shape[1], self.train_x.shape[2])
 
         self.steps_per_epoch = steps_per_epoch
         self.epochs = epochs
@@ -56,18 +48,13 @@ class Model:
         self.plot_loss = False
 
     def build_model(self):
-
-        inputs = ks.layers.Input(shape=(self.input_data.shape[1], self.input_data.shape[2]))
-        lstm_out = ks.layers.LSTM(self.state_size)(inputs)
+        inputs = ks.layers.Input(shape=self.input_shape)
+        lstm_out = ks.layers.LSTM(self.state_size, return_sequences=True)(inputs)
         outputs = ks.layers.Dense(1)(lstm_out)
 
         self.model = ks.Model(inputs=inputs, outputs=outputs)
         self.model.compile(optimizer=ks.optimizers.Adam(learning_rate=self.learning_rate), loss="mse")
         self.model.summary()
-
-    def generate_validation_data(self):
-        test_x_batch = []
-        test_y_batch = []
 
     def train(self):
         es_callback = ks.callbacks.EarlyStopping(monitor="val_loss", min_delta=0, patience=5)
@@ -82,13 +69,33 @@ class Model:
 
         callbacks = [es_callback, modelckpt_callback]
 
-        # TODO: Fit on generator
-
         val = (self.test_x, self.test_y)
         history = self.model.fit(x=self.train_x, y=self.train_y, steps_per_epoch=self.steps_per_epoch,
                                  epochs=self.epochs, validation_data=val, callbacks=callbacks)
 
         return history
+
+    def predict(self, x, plot=False, y=None):
+        # TODO: Denormalize prediction
+        # Expand dimension so that keras doesn't complain
+        x = np.expand_dims(x, axis=0)
+        print(self.input_shape, x.shape)
+        result = self.model.predict(x)[0]
+
+        if plot:
+            print(y.shape, result.shape)
+            print("y", y)
+            plt.plot(y, label="real")
+
+            print("result", result)
+            plt.plot(result, label="predicted")
+
+            plt.title(label="Prediction")
+            plt.legend()
+
+            plt.show()
+
+        return result
 
     def visualize_loss(self, history):
         loss = history.history["loss"]
@@ -104,7 +111,16 @@ class Model:
         plt.show()
 
 
-model = Model()
+# Create input dummy set, with 100 datapoints, of 24 hours, with 50 input features
+input_data = np.zeros((100, 24, 50)) + 1
+# Create output dummy set, with 100 datapoints, of 24 hours, with 1 output feature
+output_data = np.zeros((100, 24, 1)) + 2
+
+dummy_train_data = input_data[:80, :], output_data[:80, :]
+dummy_test_data = input_data[80:, :], output_data[80:, :]
+
+model = Model(training_data=dummy_train_data, validation_data=dummy_test_data, epochs=100)
 model.build_model()
 hst = model.train()
-model.visualize_loss(hst)
+# model.visualize_loss(hst)
+model.predict(dummy_train_data[0][0], plot=True, y=dummy_train_data[1][0])
